5c8613c3feea22079eb967b82c2ce383b8256795
Scott Hoisington <hoyzington@gmail.com>
2019-12-13 18:12:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,6 +18,7 @@ def monopoly_with_second_tier
   monopoly[:railroads][:pieces] = 4
   monopoly[:railroads][:names] = {}
   monopoly[:railroads][:rent_in_dollars] = {}
+  monopoly
 end
 
 def monopoly_with_third_tier
@@ -34,6 +35,7 @@ def monopoly_with_third_tier
   monopoly[:railroads][:names][:pennsylvania_railroad] = {}
   monopoly[:railroads][:names][:b_and_o_railroad] = {}
   monopoly[:railroads][:names][:shortline] = {}
+  monopoly
 end
 
 def monopoly_with_fourth_tier
@@ -54,6 +56,7 @@ def monopoly_with_fourth_tier
   monopoly[:railroads][:names][:pennsylvania_railroad]["mortgage_value"] = "$200"
   monopoly[:railroads][:names][:b_and_o_railroad]["mortgage_value"] = "$400"
   monopoly[:railroads][:names][:shortline]["mortgage_value"] = "$800"
+  monopoly
 end
 
 
